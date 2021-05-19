948dc9aa4313a67566f5345eaa0451169c3fa4a9
juanarrieta1986 <juanca86@gmail.com>
2021-05-19 20:57:00 +0000

    v2

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -12,7 +12,10 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			colorRojo: ["rgb(104, 38, 38)"],
+			colorAmarillo: ["rgb(121, 121, 50)"],
+			colorVerde: ["rgb(56, 121, 56)"]
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
@@ -37,6 +40,24 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
+			},
+			redColor: () => {
+				const store = getStore();
+				if ((store.colorRojo[0] = "rgb(104, 38, 38)")) {
+					setStore({ colorRojo: "#FF0000" });
+				} else setStore({ colorRojo: "rgb(104, 38, 38)" });
+			},
+			yellowColor: () => {
+				const store = getStore();
+				if ((store.colorAmarillo[0] = "rgb(104, 38, 38)")) {
+					setStore({ colorAcolorVerdemarillo: "#FF0000" });
+				} else setStore({ colorVerde: "rgb(104, 38, 38)" });
+			},
+			greenColor: () => {
+				const store = getStore();
+				if ((store.colorVerde[0] = "rgb(104, 38, 38)")) {
+					setStore({ colorVerde: "#FF0000" });
+				} else setStore({ colorVerde: "rgb(104, 38, 38)" });
 			}
 		}
 	};
